ecb5b47fe1db9223cd97fde2c9c504a651652519
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-10 17:25:13 +0200

    Handle static assets serving and add stylesheets
    - Static assets are now served in the public
    folder
    - Add index.css and form.css with basic styling

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require("node:path");
 const PORT = 3000;
 const indexRouter = require("./routes/indexRouter");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
