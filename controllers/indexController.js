9e98df5b4e8060e752813a6a428732bb135ddaf5
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-10-10 14:39:22 +0200

    Render messages in index view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,18 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 module.exports = {
   get: (req, res) => {
-    res.render("index");
+    res.render("index", { title: "Mini Messageboard", messages: messages });
   },
 };
